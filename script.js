9c8b200e1fce37ec401fb02b7b8f310f7cc4863e
Azekas <106997534+Azekas@users.noreply.github.com>
2022-07-19 10:21:29 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+"use strict"
+
+do {
+    var kaina = prompt("Įveskite prekės kainą :)")
+    kaina = Number(kaina)
+} while(isNaN(kaina)) {
+    var bepvm = kaina / 121 * 100
+    var pvm = kaina - bepvm
+    var kaina1 = kaina.toFixed(2)
+    var pvm1 = pvm.toFixed(2)
+    var bepvm1 = bepvm.toFixed(2)
+}
+
+console.log("Prekės kaina su PVM: " + kaina1 + " €")
+console.log("PVM: " + pvm1 + " €")    
+console.log("Prekės kaina be PVM: " + bepvm1 + " €")
